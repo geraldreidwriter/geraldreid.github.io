64cc1be2a1826daf42c74b2578d897cb41045723
geraldreidwriter <geraldreidwriter@gmail.com>
2025-01-20 21:05:42 -0600

    Fixing navbar links

--- generate-from-markdown.js
+++ generate-from-markdown.js
@@ -71,8 +71,8 @@ const generatePageFromMarkdown = (markdownFilePath) => {
       <nav class="nav-menu">
         <ul>
           <li><a href="/">Home</a></li>
-          <li><a href="/poetry">Poetry</a></li>
-          <li><a href="/stories">Stories</a></li>
+          <li><a href="/poetry/poetry-archive.html">Poetry</a></li>
+          <li><a href="/stories/story-archive.html">Stories</a></li>
         </ul>
       </nav>
     </div>
@@ -152,8 +152,8 @@ const generateIndexPage = (type, items) => {
       <nav class="nav-menu">
         <ul>
           <li><a href="/">Home</a></li>
-          <li><a href="/poetry">Poetry</a></li>
-          <li><a href="/stories">Stories</a></li>
+          <li><a href="/poetry/poetry-archive.html">Poetry</a></li>
+          <li><a href="/stories/story-archive.html">Stories</a></li>
         </ul>
       </nav>
     </div>
